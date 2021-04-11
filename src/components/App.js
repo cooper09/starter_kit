542352dbd4d1e53051102fe5b0a58e9935cd8855
cooper09 <cooperatkmba@gmail.com>
2021-04-11 17:08:57 -0400

    commit with js blockchain

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
 
+import '../blockchain.js';
+
 class App extends Component {
   render() {
     return (
